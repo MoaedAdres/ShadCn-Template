2655cf4baac14665b021786aeea93ec72dc99a98
moaed <moaedadres22@gmail.com>
2024-11-16 17:54:25 +0300

    fix context

--- src/hooks/use-theme.tsx
+++ src/hooks/use-theme.tsx
@@ -1,12 +1,16 @@
 import { useContext } from "react";
-import { DashboardContext, ThemeContextType } from '@/Views/Dashboard/Dashboard'
+import { DashboardContext } from "@/Views/Dashboard/Dashboard";
 
+type ThemeContextType = {
+  theme: string;
+  toggleTheme: (newTheme: string) => void;
+};
 export const useTheme = (): ThemeContextType => {
-    const context = useContext(DashboardContext);
+  const context = useContext(DashboardContext);
 
-    if (!context) {
-        throw new Error("useTheme must be used within a ThemeProvider");
-    }
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
 
-    return context;
+  return context;
 };
